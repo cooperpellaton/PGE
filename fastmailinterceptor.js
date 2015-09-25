e5ae98ec453df49e19bf2f001f67a067e8c97299
Cooper Pellaton <c@cepp.at>
2015-09-25 00:49:53 -0400

    Adding in-line encryption and decryption as well as a user lookup from Keybase.io.

--- fastmailinterceptor.js
+++ fastmailinterceptor.js
@@ -58,27 +58,82 @@ var transform = function(data) {
     if (resp[i][0] === "messageDetails") {
       var detailsList = resp[i][1].detailsList;
       for (var j = detailsList.length; j--;) {
-        detailsList[j].body = detailsList[j].body.replace(" ", $.getScript("encryptor.js", function()) {
-          alert("The encryption scheme has been loading but cannot be assumed to work at this point.")
-        });
-        console.log(detailsList[j].body)
+        detailsList[j].body = decrypt(detailsList[j].body);
       }
     }
   }
   return JSON.stringify(resp);
 }
 
-/*
-var oldWindowEventSource = window.EventSource;
-window.EventSource = function(url) {
-    console.log(url);
-    var source = new oldWindowEventSource(url);
-    return {
-        addEventListener: function(n, f, b) {
-            source.addEventListener(n, new function(e) {
-                console.log(arguments);
-            }, b);
-        }
+var decrypt = function(s) {
+    var options = {
+      numBits: 2048,
+      function generateTrueUser = (user) {
+        var userSearch = https: //keybase.io/_/api/1.0/user/lookup.json + "user";
+          Get userSearch;
+        var actualUser = them.username;
+      }
+      userId: actualUser,
+      passphrase: password,
     };
-};
-*/
+
+    if {
+      function(Get userSearch === null) {
+        openpgp.generateKeyPair(options).then(function(keypair) {
+          // success
+          var privkey = keypair.privateKeyArmored;
+          var pubkey = keypair.publicKeyArmored;
+        }).catch(function(error) {
+          // failure
+        });
+      }
+    } else {
+      var pubkey = get UserSearch.public_keys;
+    }
+    var encryptionTester = function() {
+        var contents = body.search("BEGIN PGP");
+        if (contents = 0) {
+          //Encrypting the body.
+          var openpgp = require('openpgp');
+
+          var key = '-----BEGIN PGP PUBLIC KEY BLOCK ... END PGP PUBLIC KEY BLOCK-----';
+          var publicKey = openpgp.key.readArmored(key);
+
+          openpgp.encryptMessage(publicKey.keys, 'Hello, World!').then(function(pgpMessage) {
+            // success
+          }).catch(function(error) {
+            // failure
+          });
+        } else {
+          //Decrypting the body.
+          var openpgp = require('openpgp');
+          return {
+            var key = '-----BEGIN PGP PRIVATE KEY BLOCK ... END PGP PRIVATE KEY BLOCK-----';
+            var privateKey = openpgp.key.readArmored(key).keys[0];
+            privateKey.decrypt('passphrase');
+
+            var pgpMessage = '-----BEGIN PGP MESSAGE ... END PGP MESSAGE-----';
+            pgpMessage = openpgp.message.readArmored(pgpMessage);
+
+            openpgp.decryptMessage(privateKey, pgpMessage).then(function(plaintext) {
+              // success
+            }).catch(function(error) {
+              // failure
+            });
+          }
+        }
+      }
+      /*
+      var oldWindowEventSource = window.EventSource;
+      window.EventSource = function(url) {
+          console.log(url);
+          var source = new oldWindowEventSource(url);
+          return {
+              addEventListener: function(n, f, b) {
+                  source.addEventListener(n, new function(e) {
+                      console.log(arguments);
+                  }, b);
+              }
+          };
+      };
+      */
